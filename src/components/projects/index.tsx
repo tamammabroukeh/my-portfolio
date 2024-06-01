a99291ce6f4a1f15e6eb29936dd07aadfb231df4
tamammabroukeh <tamammb367@@gmail.com>
2024-06-01 16:51:56 -0700

    fix data

--- src/components/projects/index.tsx
+++ src/components/projects/index.tsx
@@ -3,28 +3,28 @@ import { motion } from "framer-motion";
 import ProjectLayout from "./ProjectLayout";
 import { IProjectData, IProjectsData } from "@/interfaces/interfaces";
 const container = {
-    hidden: { opacity: 0 },
-    show: {
-        opacity: 1,
-        transition: {
-            staggerChildren: 0.3,
-            delayChildren: 1.5,
-        },
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 1,
+      delayChildren: 1.5,
     },
+  },
 };
 
 const ProjectList = ({ projects }: IProjectsData) => {
-    return (
-        <motion.div
-            variants={container}
-            initial="hidden"
-            animate="show"
-            className="w-full px-0 xs:px-3 grid gap-7 grid-cols-1 lg:grid-cols-2 place-items-center lg:place-items-stretch"
-        >
-            {projects.map((project: IProjectData, index: number) => {
-                return <ProjectLayout key={index} {...project} />;
-            })}
-        </motion.div>
-    );
+  return (
+    <motion.div
+      variants={container}
+      initial="hidden"
+      animate="show"
+      className="w-full px-0 xs:px-3 grid gap-7 grid-cols-1 lg:grid-cols-2 place-items-center lg:place-items-stretch"
+    >
+      {projects.map((project: IProjectData, index: number) => {
+        return <ProjectLayout key={index} {...project} />;
+      })}
+    </motion.div>
+  );
 };
 export default ProjectList;
