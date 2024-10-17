0cef72e9bcd1adb59f31eaaf246c23a7d0da56b3
tamammabroukeh <tamammb367@@gmail.com>
2024-10-17 00:32:18 -0700

    new cv

--- src/data/personal.ts
+++ src/data/personal.ts
@@ -1,34 +1,34 @@
 import { IBtnList } from "@/interfaces/interfaces";
 
 export const BtnList: IBtnList[] = [
-    { label: "Home", link: "/", icon: "home", newTab: false },
-    { label: "About", link: "/about", icon: "about", newTab: false },
-    { label: "Projects", link: "/projects", icon: "projects", newTab: false },
-    { label: "Contact", link: "/contact", icon: "contact", newTab: false },
-    {
-        label: "Github",
-        link: "https://github.com/tamammabroukeh/",
-        icon: "github",
-        newTab: true,
-    },
-    {
-        label: "LinkedIn",
-        link: "https://www.linkedin.com/in/tamam-mabrouka-a0aa37224/",
-        icon: "linkedin",
-        newTab: true,
-    },
-    {
-        label: "Gitlab",
-        link: "https://gitlab.com/tammammabroukeh/",
-        icon: "gitlab",
-        newTab: true,
-    },
-    {
-        label: "Resume",
-        link: "/Tammam Mabroukeh.pdf",
-        icon: "resume",
-        newTab: true,
-    },
+  { label: "Home", link: "/", icon: "home", newTab: false },
+  { label: "About", link: "/about", icon: "about", newTab: false },
+  { label: "Projects", link: "/projects", icon: "projects", newTab: false },
+  { label: "Contact", link: "/contact", icon: "contact", newTab: false },
+  {
+    label: "Github",
+    link: "https://github.com/tamammabroukeh/",
+    icon: "github",
+    newTab: true,
+  },
+  {
+    label: "LinkedIn",
+    link: "https://www.linkedin.com/in/tamam-mabrouka-a0aa37224/",
+    icon: "linkedin",
+    newTab: true,
+  },
+  {
+    label: "Gitlab",
+    link: "https://gitlab.com/tammammabroukeh/",
+    icon: "gitlab",
+    newTab: true,
+  },
+  {
+    label: "Resume",
+    link: "/Tammam Mabroukeh Frontend.pdf",
+    icon: "resume",
+    newTab: true,
+  },
 ];
 export const NAME = "Tammam Mabroukeh"
 export const NUMBER_OF_CLIENTS = "+3"
